3ab6504e95e721b7733770380cd57374c9fc6438
zhangjie <zhangjie@163.com>
2021-07-21 18:37:27 +0800

    新增的测试

---
+++ src/main/java/com/bjpowernode/Test.java
@@ -0,0 +1,26 @@
+package com.bjpowernode;
+
+/**
+ * 张杰
+ * 2021/7/21
+ */
+public class Test {
+    public static void main(String[] args) {
+        int[] arrays = {52,32,22,5,10,35,8,6};
+        Bubble(arrays);
+        for(int m = 0; m < arrays.length; m++){
+            System.out.print(arrays[m] + " ");
+        }
+    }
+    public static void Bubble(int[] a){
+        for(int i = 1; i < a.length; i++){
+            for(int k = 0; k < a.length - i; k++){
+                if(a[k] > a[k+1]){
+                    int temp = a[k+1];
+                    a[k+1] = a[k];
+                    a[k] = temp;
+                }
+            }
+        }
+    }
+}
